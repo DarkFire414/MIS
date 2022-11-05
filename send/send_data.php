be9858ea64657542279aa0dd194ccd94476ed573
DarkFire414 <joahan_hernandez@outlook.com>
2022-11-05 05:31:36 -0600

    Final structure of the table defined

--- send/send_data.php
+++ send/send_data.php
@@ -11,14 +11,15 @@ mysqli_select_db($conection, $nameDb);
 //Receive data, method post
 $lab = $_GET ['lab'];
 $mov = $_GET ['mov'];
+$count = $_GET ['count'];
 
 //Message to client
 echo "  Lab: \n"; echo $lab;
 echo "  Mov: \n"; echo $mov;
 
-$query = "INSERT INTO `movimientos` (`id`, `laboratorio`, `movimiento`, `fecha`) VALUES (NULL, '$lab', '$mov', current_timestamp(2));";
+$query = "INSERT INTO `movimientos` (`id`, `laboratorio`, `movimiento`, `conteo`, `fecha`) VALUES (NULL, '$lab', '$mov', '$count', current_timestamp(2));";
 mysqli_query($conection, $query);
 mysqli_close($conection);
 echo " Data received \n";
-echo $query;
+//echo $query;
 ?>
